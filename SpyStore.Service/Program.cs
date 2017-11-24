85686112a6d8ecb7787f592e6d08cfdd5668e33b
Jeong H Park <hello@jeonghyuk.info>
2017-11-24 16:27:37 -0500

    Ch03 - MVC Projects and Files (completed p.130 of 418) -Program.cs File -appsettings.json File -runtimeconfig.template.json File -Startup.cs File -Controllers Folder -wwwroot Folder -web.config File -launchsettings.json File

--- SpyStore.Service/Program.cs
+++ SpyStore.Service/Program.cs
@@ -14,7 +14,17 @@ namespace SpyStore.Service
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:40001/")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- SpyStore.Service/Startup.cs
+++ SpyStore.Service/Startup.cs
@@ -1,18 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SpyStore.DAL.EF;
+using SpyStore.DAL.Initializers;
+using SpyStore.DAL.Repos;
+using SpyStore.DAL.Repos.Interfaces;
 
 namespace SpyStore.Service
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            _env = env;
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+                loggerFactory.AddDebug();
+
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    StoreDataInitializer.InitializeData(app.ApplicationServices);
+                }
+            }
+
+            app.UseCors("AllowAll");    // UseCors() has to go before UseMvc()
+            app.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services
+            services.AddMvcCore()
+                    .AddJsonFormatters(j =>
+                    {
+                        j.ContractResolver = new DefaultContractResolver();
+                        j.Formatting = Formatting.Indented;
+                    });
+
+            // Allow CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials();
+                });
+            });
+
+            // Add EntityFramework
+            services.AddDbContext<StoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SpyStore")));
+
+            services.AddScoped<ICategoryRepo, CategoryRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IShoppingCartRepo, ShoppingCartRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderDetailRepo, OrderDetailRepo>();
+        }
+
+        #region Default StartUp Codes
+
+        /*
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -36,5 +105,8 @@ namespace SpyStore.Service
 
             app.UseMvc();
         }
+        */
+
+        #endregion Default StartUp Codes
     }
 }
