a8a18a71860d6b91f4351a5c7e61dc3f67b4acdd
Jeong H Park <hello@jeonghyuk.info>
2017-12-14 16:45:43 -0500

    Ch04 - Adding the View Models (completed p.184 of 418)

---
+++ SpyStore.MVC/ViewModels/Base/CartViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using SpyStore.Models.ViewModels.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpyStore.MVC.ViewModels.Base
+{
+    public class CartViewModelBase : ProductAndCategoryBase
+    {
+        public int? CustomerId { get; set; }
+
+        [DataType(DataType.Currency), Display(Name = "Total")]
+        public decimal LineItemTotal { get; set; }
+
+        public string TimeStampString => TimeStamp != null ? JsonConvert.SerializeObject(TimeStamp).Replace("\"", "") : string.Empty;
+    }
+}
