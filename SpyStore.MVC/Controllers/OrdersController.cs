d5de9fdfe581ae4ecd5a37a74b815a25e26ac412
Jeong H Park <hello@jeonghyuk.info>
2018-05-26 15:34:22 -0400

    Ch05 - Building the Controllers (completed p.204 of 418) -The Proudcts Controller -The Orders Controller -The Shopping Cart Controller

---
+++ SpyStore.MVC/Controllers/OrdersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpyStore.Models.Entities;
+using SpyStore.Models.ViewModels;
+using SpyStore.MVC.WebServiceAccess.Base;
+
+namespace SpyStore.MVC.Controllers
+{
+    [Route("[controller]/[action]/{customerId}")]
+    public class OrdersController : Controller
+    {
+        private readonly IWebApiCalls _webApiCalls;
+
+        public OrdersController(IWebApiCalls webApiCalls)
+        {
+            _webApiCalls = webApiCalls;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int customerId)
+        {
+            ViewBag.Title = "Order History";
+            ViewBag.Header = "Order History";
+
+            IList<Order> orders = await _webApiCalls.GetOrdersAsync(customerId);
+
+            if (orders == null)
+                return NotFound();
+
+            return View(orders);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int customerId, int orderId)
+        {
+            ViewBag.Title = "Order Details";
+            ViewBag.Header = "Order Details";
+
+            var orderDetails = await _webApiCalls.GetOrderDetailsAsync(customerId, orderId);
+
+            if (orderDetails == null)
+                return NotFound();
+
+            return View(orderDetails);
+        }
+    }
+}
