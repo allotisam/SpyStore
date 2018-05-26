99de45a23ca6d0c73ae2a276a4fc0c40f784549e
Jeong H Park <hello@jeonghyuk.info>
2018-05-26 18:09:31 -0400

    Ch05 - Validations (completed p.211 of 418) -Server Side Validation -Client Side VAlidation -Updating the View Models

--- SpyStore.MVC/ViewModels/AddToCartViewModel.cs
+++ SpyStore.MVC/ViewModels/AddToCartViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using SpyStore.MVC.ViewModels.Base;
+using SpyStore.MVC.Validations;
 
 namespace SpyStore.MVC.ViewModels
 {
     public class AddToCartViewModel : CartViewModelBase
     {
+        [MustBeGreaterThanZero]
+        [MustNotBeGreaterThan(nameof(UnitsInStock))]
         public int Quantity { get; set; }
     }
 }
